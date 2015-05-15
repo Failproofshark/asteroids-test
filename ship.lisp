43ba0470ff8dbc9f38f2fc0dfb348195afafe154
Bryan Baraoidan <bryan.baraoidan@gmail.com>
2015-05-15 15:43:17 -0700

    Added a velocity vector to the ship to improve the movement of the ship. Also added a line to represent the direction the ship is currently facing

--- ship.lisp
+++ ship.lisp
@@ -7,7 +7,8 @@
         :asteroids.entities.math-vector)
   (:export :ship
            :update
-           :handle-keydown-input))
+           :handle-keydown-input
+           :handle-keyup-input))
 
 (in-package :asteroids.entities.ship)
 
@@ -20,17 +21,23 @@
     :initform 30)
    (height
     :initform 30)
+   (velocity
+    :initform (make-instance 'math-vector)
+    :accessor velocity)
    ;;Yes technically acceleration is a vector but for our purposes simply knowing the magnitude is ok.
    (acceleration
     :initform (make-instance 'math-vector)
     :accessor acceleration)
-   ;;Everything is upside down
+   ;;Facing up
    (rotation-angle
-    :initform 270.0
+    :initform 90.0
     :accessor rotation-angle)))
 
 (defgeneric handle-keydown-input (Ship scancode)
   (:documentation "Handle keydown events from the user"))
+
+(defgeneric handle-keyup-input (Ship scancode)
+  (:documentation "Handle keyup events from the user"))
 
 (defgeneric update (ship)
   (:documentation "An update routine for any possible changes to it's position and perhaps more"))
@@ -38,15 +45,23 @@
 (defmethod handle-keydown-input ((ship ship) scancode)
   (with-accessors ((rotation-angle rotation-angle) (acceleration acceleration) (x x) (y y)) ship
     (cond
-      ((scancode= scancode :scancode-left) (decf rotation-angle 2))
-      ((scancode= scancode :scancode-right) (incf rotation-angle 2))
+      ((scancode= scancode :scancode-left) (incf rotation-angle 2))
+      ((scancode= scancode :scancode-right) (decf rotation-angle 2))
       ((scancode= scancode :scancode-up) (progn (setf (direction acceleration) rotation-angle)
-                                                (incf (magnitude acceleration) 0.5))))))
+                                                (incf (magnitude acceleration) 0.005))))))
+
+(defmethod handle-keyup-input ((ship ship) scancode)
+  (with-accessors ((acceleration acceleration)) ship
+    (cond
+      ((scancode= scancode :scancode-up) (setf (magnitude acceleration) 0)))))
 
 (defmethod update ((ship ship))
-  (with-accessors ((x x) (y y) (rotation-angle rotation-angle) (acceleration acceleration)) ship
-      (incf x (get-x-component acceleration))
-      (incf y (get-y-component acceleration))))
+  (with-accessors ((x x) (y y) (rotation-angle rotation-angle) (acceleration acceleration) (velocity velocity)) ship
+    (multiple-value-bind (resultant-x resultant-y resultant-magnitude resultant-angle) (vector+ velocity acceleration)
+      (setf (magnitude velocity) resultant-magnitude)
+      (setf (direction velocity) resultant-angle)
+      (incf x resultant-x)
+      (incf y resultant-y))))
 
 (defmethod draw ((ship ship))
   (with-accessors ((rotation-angle rotation-angle)) ship
--- vector.lisp
+++ vector.lisp
@@ -1,5 +1,6 @@
 (in-package :cl-user)
 (defpackage :asteroids.entities.math-vector
+  (:documentation "A small helper library for vectors *should only be used for 2d maths*")
   (:use :cl
         :asteroids.helpers)
   (:export :math-vector
@@ -7,10 +8,12 @@
            :magnitude
            :get-x-component
            :get-y-component
-           :get-vector-components))
+           :get-vector-components
+           :vector+))
 (in-package :asteroids.entities.math-vector)
 ;; Named to distinguish itself from the vector data structure
 (defclass Math-Vector ()
+  ;; IN DEGREES!
   ((direction
     :initarg :direction
     :initform 0
@@ -25,6 +28,10 @@
   (:documentation "Returns the y vector component"))
 (defgeneric get-vector-components (math-vector)
   (:documentation "Returns the x and y vector components of the vector as a list"))
+(defgeneric vector+ (math-vector math-vector)
+  (:documentation "Perform vector addition"))
+(defgeneric dot-product (math-vector math-vector)
+  (:documentation "Find the dot product between two vectors"))
 
 (defmethod get-x-component ((math-vector math-vector))
   (* (magnitude math-vector) (cos (degrees-to-radians (direction math-vector)))))
@@ -34,3 +41,18 @@
 
 (defmethod get-math-vector-components ((math-vector math-vector))
   `(,(get-x-component math-vector) ,(get-y-component math-vector)))
+
+(defmethod vector+ ((math-vector-1 math-vector) (math-vector-2 math-vector))
+  "Usually vector addition gives a new veector, however since we're going to need the x and y components of the resultant vector we give report along with the resultant vector"
+  (let* ((vector-1-x (get-x-component math-vector-1))
+         (vector-1-y (get-y-component math-vector-1))
+         (vector-2-x (get-x-component math-vector-2))
+         (vector-2-y (get-y-component math-vector-2))
+         (resultant-vector-x-component (+ vector-1-x vector-2-x))
+         (resultant-vector-y-component (+ vector-1-y vector-2-y))
+         (resultant-magnitude (sqrt (+ (expt resultant-vector-x-component 2) (expt resultant-vector-y-component 2))))
+         (resultant-angle (radians-to-degrees (atan resultant-vector-y-component resultant-vector-x-component))))
+    (values resultant-vector-x-component
+            resultant-vector-y-component
+            resultant-magnitude
+            resultant-angle)))
